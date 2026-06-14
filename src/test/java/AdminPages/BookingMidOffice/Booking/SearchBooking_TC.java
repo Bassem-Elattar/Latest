dd6d10b8aafb006beba20db3404932d6949e5b3b
ahmedrefat-prog <ahmedref124@gmail.com>
2026-06-14 11:29:44 +0300

    Flight booking structure

--- src/test/java/AdminPages/BookingMidOffice/Booking/SearchBooking_TC.java
+++ src/test/java/AdminPages/BookingMidOffice/Booking/SearchBooking_TC.java
@@ -13,63 +13,60 @@ import org.testng.annotations.Test;
 import utilities.DataUtils;
 
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Map;
 
 public class SearchBooking_TC extends TestBase_TC {
 
-   private SearchBookingBranch Booking;
-   private LogIn_Page logIn;
-    //DashboardPage dashObj;
+    private SearchBookingBranch Booking;
+    private LogIn_Page logIn;
     Faker faker = new Faker();
     SHAFT.TestData.JSON testData;
-
-    String NumberOfAdults = DataUtils.getJsonData("searchBookingBrData", "NumberOfAdults");
-    String NumberOfChildren = DataUtils.getJsonData("searchBookingBrData", "NumberOfChildren");
-    String NumberOfInfants = DataUtils.getJsonData("searchBookingBrData", "NumberOfInfants");
-    String source = DataUtils.getJsonData("searchBookingBrData", "source");
-    String destination = DataUtils.getJsonData("searchBookingBrData", "destination");
-    String BranchName = DataUtils.getJsonData("searchBookingBrData", "brName");
-    String dayOfFirstJourney = DataUtils.getJsonData("searchBookingBrData", "JourneyDay");
-    String monthOfFirstJourney = DataUtils.getJsonData("searchBookingBrData", "JourneyMonth");
-    String yearOfFirstJourney = DataUtils.getJsonData("searchBookingBrData", "JourneyYear");
-    String searchLimit = DataUtils.getJsonData("SearchBooking", "searchLimit");
 
     public SearchBooking_TC() throws FileNotFoundException {
     }
 
     @BeforeTest
-   public void sign(){
-       logIn = new LogIn_Page(driver);
-       logIn.ClickAdmin();
-       logIn.ClickOnLoginButton();
-       Booking = new SearchBookingBranch(driver);
-       testData = new SHAFT.TestData.JSON("searchBookingBrData.json");
-   }
+    public void sign(){
+        logIn = new LogIn_Page(driver);
+        logIn.ClickAdmin();
+        logIn.ClickOnLoginButton();
+        Booking = new SearchBookingBranch(driver);
+        testData = new SHAFT.TestData.JSON("searchBookingBrData.json");
+    }
 
     @Test
     public void SearchOneWay() throws InterruptedException {
         SearchBookingBranch searchBookingBranch = new SearchBookingBranch(driver);
         new Booking_Common(driver).clickBookingMidOffice();
         //new SearchBooking_Page(driver).
-        searchBookingBranch.SelectBranch(BranchName).
-                AddStartingFrom(source).AddGoingTo(destination)
-                .SelectDateOfJourney(dayOfFirstJourney, yearOfFirstJourney, monthOfFirstJourney)
+        searchBookingBranch.SelectBranch(testData.getTestData("brName")).
+                AddStartingFrom(testData.getTestData("source")).AddGoingTo(testData.getTestData("destination"))
+                .SelectDateOfJourney(testData.getTestData("JourneyDay"), testData.getTestData("JourneyYear"), testData.getTestData("JourneyMonth"))
                 .passengersDropDown()
-                .SelectNumberOfAdult(Integer.parseInt(NumberOfAdults)).SelectNumberOfChildren(Integer.parseInt(NumberOfChildren)).SelectNumberOfInfant(Integer.parseInt(NumberOfInfants)).clickOnSearchButton();
+                .SelectNumberOfAdult(Integer.parseInt(testData.getTestData("NumberOfAdults"))).SelectNumberOfChildren(Integer.parseInt(testData.getTestData("NumberOfChildren"))).SelectNumberOfInfant(Integer.parseInt(testData.getTestData("NumberOfInfants"))).clickOnSearchButton().OpenSideMenuInfo();
+        List<String> SegmentData = searchBookingBranch.SegmentDetails();
+        List<String> FareData = searchBookingBranch.FareDetails();
+        List<String> BaggageData = searchBookingBranch.BaggageInfo();
+        searchBookingBranch.CloseTheSideMenuInfo();
+        List<String> FlightCard = searchBookingBranch.FlightCard();
+        searchBookingBranch.BookFirstFlight();
+        List<String> paxSegmentDetails = searchBookingBranch.PaxSegmentDetails();
+        System.out.println(paxSegmentDetails);
 
-        JavascriptExecutor js = (JavascriptExecutor) driver.getDriver();
-
-// This script returns true if an element containing that exact text exists on the page
-        Thread.sleep(9000);
-        boolean noResults = (Boolean) js.executeScript(
-                "return Array.from(document.querySelectorAll('p')).some(p => p.textContent.includes('No, search results available.'));"
-        );
-
-        if (noResults) {
-            System.out.println("No search results message");
-            searchBookingBranch.clickOnSearchButton();
-        } else {
-            System.out.println("There is search results");
-        }
+//        JavascriptExecutor js = (JavascriptExecutor) driver.getDriver();
+//
+//// This script returns true if an element containing that exact text exists on the page
+//        Thread.sleep(9000);
+//        boolean noResults = (Boolean) js.executeScript(
+//                "return Array.from(document.querySelectorAll('p')).some(p => p.textContent.includes('No, search results available.'));"
+//        );
+//
+//        if (noResults) {
+//            System.out.println("No search results message");
+//            searchBookingBranch.clickOnSearchButton();
+//        } else {
+//            System.out.println("There is search results");
+//        }
     }
 }
